275eb71f7fc971410ebffe657fbf22a8a5c2c1e2
scor4er <scor4er@gmail.com>
2016-01-20 00:32:54 +0300

    Plugin base structure without EMDK references

---
+++ www/emdk.js
@@ -0,0 +1,17 @@
+cordova.define("com.itretail.motorola", function(require, exports, module) { /*
+
+/*global cordova, module*/
+
+module.exports = {
+    start: function () {
+        cordova.exec(null, null, 'MotorolaScanner', 'start', []);
+    },
+    registerForBarcode: function (callback) {
+        cordova.exec(callback, null, 'MotorolaScanner', 'register', []);
+    },
+    trigger: function () {
+        cordova.exec(null, null, 'MotorolaScanner', 'trigger', []);
+    }
+};
+
+});
